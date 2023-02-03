c734dd8d2fc1d5d02138e1bcae5c413a178b91dd
David <Rigby335@hotmail.com>
2023-02-03 15:46:39 -0800

    practice using useState and passing props

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,27 @@ import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
 
 import './Home.css';
+import { useState } from 'react';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
+  const [text, setText] = useState('');
+  const [font, setFont] = useState('');
+  const [align, setAlign] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} subTitle={subTitle} text={text} font={font} align={align} />
+      <Editor
+        setTitle={setTitle}
+        setSubTitle={setSubTitle}
+        setText={setText}
+        setFont={setFont}
+        setAlign={setAlign}
+      />
     </main>
   );
 }
